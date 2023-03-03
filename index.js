03a87b2a59a05c8c4b906b7746ccfd1bd6cd83b1
tommyWrightWeb <114081909+tommyWrightWeb@users.noreply.github.com>
2023-03-03 16:57:59 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const fs = require('fs')
 
 const app = express();
 
-
+app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 app.use(express.json())
 
 app.get('/', function (req, res) {
-   res.render('tvSearch');
+   res.render('tvSearch.ejs');
 })
 
 
