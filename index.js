990a0c9e39fe4c210aaaa9468a37b86529622a8c
tommyWrightWeb <114081909+tommyWrightWeb@users.noreply.github.com>
2023-03-03 16:38:55 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ let express = require('express')
 var app = express();
 // Render static files
 app.use(express.static('public'));
+
+app.get("/tvSearch", function (req, res) {
+   res.render("/tvSearch.html");
+})
 // Port website will run on
 app.listen(8080);
